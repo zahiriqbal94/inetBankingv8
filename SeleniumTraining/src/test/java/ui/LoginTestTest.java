07d19bc9bd72efc9f0bda98e0945dc1b8711cd78
Zahir Iqbal <zahiriqbal94@gmail.com>
2024-04-27 15:58:27 +0500

    Zahir: add udating files in gitub

---
+++ SeleniumTraining/src/test/java/ui/LoginTestTest.java
@@ -0,0 +1,35 @@
+package ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTestTest {
+	public static String browser = "chrome"; // External Configuration-XLS, CSV
+	public static WebDriver driver; // "WebDriver" is a interface and "driver" is a global reference variable
+  @Test
+  public void login() {
+	  if (browser.equals("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver(); // by taking the reference of WebDriver to implement the class "FirefoxDriver"
+		} else if (browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver(); // by taking the reference of WebDriver to implement the class "ChromeDriver"
+		} else if (browser.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver(); // by taking the reference of WebDriver to implement the Class "EdgeDriver"
+		}
+
+		driver.get("http://www.saucedemo.com/");
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div[1]/div/div/form/input")).click();
+		driver.close();
+
+	}
+  }
